7ab5bdba722c868ea14fc311204d4c0d458131f1
j <j@J7BF4>
2024-01-03 19:28:30 -0600

    update

--- SeqSerilogExample2/Controllers/WeatherForecastController.cs
+++ SeqSerilogExample2/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Serilog;
 
 namespace SeqSerilogExample2.Controllers
 {
@@ -21,13 +22,18 @@ namespace SeqSerilogExample2.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+
+            Log.Information("Weather Forecasts => {@result}", result);
+
+
+            return result;
         }
     }
 }
--- SeqSerilogExample2/Program.cs
+++ SeqSerilogExample2/Program.cs
@@ -1,6 +1,22 @@
+using SeqSerilogExample2;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// SERILOG CONFIGURATION
+// builder.Host.UseSerilog((context, loggerConfig) => loggerConfig.ReadFrom.Configuration(context.Configuration));
+
+//Log.Logger = new LoggerConfiguration()
+//    .MinimumLevel.Information()
+//    .WriteTo.Console()
+//    .WriteTo.File("logs/mylogs-.txt", rollingInterval: RollingInterval.Day)
+//    .CreateLogger();
+
+var assemblyName = typeof(Program).Assembly.GetName().Name;
+SerilogSeqConfigHelper.Configure(assemblyName);
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
